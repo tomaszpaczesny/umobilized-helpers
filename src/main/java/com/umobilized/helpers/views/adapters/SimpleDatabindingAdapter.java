a76ac96c44c35e5669c7f25eb787b5c440061c8c
Tomasz Paczesny <tomasz.paczesny@gmail.com>
2017-06-19 13:07:42 +0200

    Points history initial screen

--- src/main/java/com/umobilized/helpers/views/adapters/SimpleDatabindingAdapter.java
+++ src/main/java/com/umobilized/helpers/views/adapters/SimpleDatabindingAdapter.java
@@ -1,21 +1,47 @@
 package com.umobilized.helpers.views.adapters;
 
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 
 import java.util.List;
 
 /**
- * Abstract adapter for presenting simple list of data.
+ * Generic adapter for presenting simple list of data.
  * It assumes usage of databinding, do it has view holder holding databinding object.
  *
  * Created by tpaczesny on 2017-02-20.
  */
-public abstract class SimpleDatabindingAdapter<T> extends RecyclerView.Adapter<DatabindingViewHolder> {
+public class SimpleDatabindingAdapter<T> extends RecyclerView.Adapter<DatabindingViewHolder> {
 
     protected final List<T> mData;
+    private final int mItemRedId;
+    private final int mViewModelVarId;
 
+
+    /**
+     * Constructor providing initial data only. Uset his constructor only when extending this class.
+     * Remember to provide own implementation of onCreateViewHolder and onBindViewHolder
+     * @param initialData
+     */
     public SimpleDatabindingAdapter(List<T> initialData) {
         mData = initialData;
+        mItemRedId = -1;
+        mViewModelVarId = -1;
+    }
+
+    /**
+     * Constructor that allows to provide layout id and binding variable id for simple view creation and binding.
+     * @param initialData
+     * @param itemResId
+     * @param viewModelBindingVariableId
+     */
+    public SimpleDatabindingAdapter(List<T> initialData, int itemResId, int viewModelBindingVariableId) {
+        mData = initialData;
+        mItemRedId = itemResId;
+        mViewModelVarId = viewModelBindingVariableId;
     }
 
     // Return the size of your dataset (invoked by the layout manager)
@@ -39,5 +65,25 @@ public abstract class SimpleDatabindingAdapter<T> extends RecyclerView.Adapter<D
         return mData.get(position);
     }
 
+    @Override
+    public DatabindingViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (mItemRedId == -1) {
+            throw new IllegalStateException("onCreteViewHolder must be overriden or itemResId provided");
+        }
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        ViewDataBinding binding = DataBindingUtil.inflate(inflater, mItemRedId, parent, false);
+
+        return new DatabindingViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(DatabindingViewHolder holder, int position) {
+        if (mViewModelVarId == -1) {
+            throw new IllegalStateException("onBindViewHolder must be overriden or viewModelBindingVariableId provided");
+        }
+        ViewDataBinding binding = holder.getBinding();
+        binding.setVariable(mViewModelVarId, getItem(position));
+        binding.executePendingBindings();
+    }
 
 }
